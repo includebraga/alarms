30692c559a2202f958b87dad1e0299a43f45ff2a
Luís Torres <luistorres1792@gmail.com>
2018-06-30 14:59:59 +0100

    Add siren sound

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Here you can find a set of buttons that can be used to announce the various emergency types in the sound system of the Firefighters headquarters.
 
 Deployed @ https://bve-alarms.netlify.com/
+
+Siren sound from:
+http://soundbible.com/mp3/Warning%20Siren-SoundBible.com-898272278.mp3
+respecting the following license: https://creativecommons.org/licenses/by/3.0/pt/deed.en
--- src/lib/readMessage.js
+++ src/lib/readMessage.js
@@ -1,9 +1,15 @@
+import sirenSound from "../assets/siren.mp3";
+
 const defaults = {
   volume: 1,
   rate: 1,
   pitch: 1,
   lang: "pt-PT",
 };
+
+/* eslint-disable */
+const siren = new Audio(sirenSound);
+/* eslint-enable */
 
 const readMessage = text => {
   const messageDefinition = { ...defaults, text };
@@ -14,7 +20,11 @@ const readMessage = text => {
 
   Object.assign(speech, messageDefinition);
 
-  return window.speechSynthesis.speak(speech);
+  siren.play();
+
+  return setTimeout(() => {
+    window.speechSynthesis.speak(speech);
+  }, 7000);
 };
 
 export default readMessage;
